6058c3056e79f5152e530b58bd660db83a69ada5
issa AbuAwada <67484019+issa554@users.noreply.github.com>
2024-01-05 16:41:47 +0200

    Update Page3.py ,add data base functions

--- Pages/Page3.py
+++ Pages/Page3.py
@@ -1,9 +1,8 @@
-
-#for make a Book Mark page
 import customtkinter as ck
 from PIL import Image
 from tkinter import ttk
 from CTkMessagebox import CTkMessagebox
+from db import mycursor,mydb
 
 
 class page3(ck.CTkFrame):
@@ -44,6 +43,10 @@ class page3(ck.CTkFrame):
 
         self.delete_button = ck.CTkButton(button_frame, text="Delete a Book Mark", command=self.delete_item)
         self.delete_button.place(x=600,y=100)
+        mycursor.execute("SELECT url , name FROM bookmark")
+        mybookmark = mycursor.fetchall()
+        for bookmark in mybookmark:
+            self.table.insert('','end',values=(bookmark))
 
     def add_data(self):
         self.add_window()
@@ -53,6 +56,16 @@ class page3(ck.CTkFrame):
             if (self.entry.get() and self.entry2) :
                 data = self.entry.get()
                 data2 = self.entry2.get()
+                mycursor.execute("SELECT name FROM bookmark")
+                mysite = mycursor.fetchall()
+                if (data2,) in mysite:
+                    CTkMessagebox(title="Warning Message",message="This Url is already blocked",icon="warning",fade_in_duration=5)
+                    return
+                else:
+                    sql = "INSERT INTO bookmark (name , url) VALUES (%s,%s)"
+                    mycursor.execute(sql, (data2,data))
+                    mydb.commit()
+
 
                 self.table.insert('','end',values=(data,data2))
                 self.entry.delete(0,'end')
@@ -89,11 +102,21 @@ class page3(ck.CTkFrame):
          else:  CTkMessagebox(title="Warning Message",message="Select a item Please",icon="warning",fade_in_duration=5)   
 
     def edit_window(self):
+        item = self.table.focus()
+        values = self.table.item(item, "values")
+        oldurl = values[0]
+        oldname = values[1]
         def get():
-            if (self.entry.get() and self.entry2.get()) :
-                item = self.table.focus()
-                data = self.entry.get()
-                data2 = self.entry2.get()
+            mycursor.execute("SELECT name FROM bookmark")
+            mysite = mycursor.fetchall()
+            data = self.entry.get()
+            data2 = self.entry2.get()
+            if (data2,) in mysite:
+                    CTkMessagebox(title="Warning Message",message="This Url is already blocked",icon="warning",fade_in_duration=5)
+            else:
+                update_query = "UPDATE bookmark SET name = %s, url = %s WHERE name = %s;"
+                mycursor.execute(update_query, (data2, data ,oldname ))
+                mydb.commit()
                 self.table.item(item, values=(data,data2))
                 self.entry.delete(0,'end')
                 self.entry2.delete(0,'end')
@@ -112,19 +135,23 @@ class page3(ck.CTkFrame):
 
         self.entry = ck.CTkEntry(new_window,width=200)
         self.entry.pack(padx=10, pady=10)
-
+        self.entry.insert(0,oldurl)
         entry_label2 = ck.CTkLabel(new_window,width=200,text="Enter New Name:",font=ck.CTkFont(family="Times New Roman", size=18,weight="bold"))
         entry_label2.pack(padx=10, pady=10)
 
         self.entry2 = ck.CTkEntry(new_window,width=200)
         self.entry2.pack(padx=10, pady=10)
-
+        self.entry2.insert(0,oldname)
         ok_button = ck.CTkButton(new_window, text="OK", command=get)
         ok_button.pack(padx=10, pady=10)    
 
     def delete_item(self):
          selected_item = self.table.focus()
          if selected_item:
+            values = self.table.item(selected_item, "values")
+            oldname = values[0]
+            sql = "DELETE FROM  bookmark  WHERE name = %s"
+            mycursor.execute(sql, (oldname,))
+            mydb.commit() 
             self.table.delete(selected_item)
          else:  CTkMessagebox(title="Warning Message",message="Select a URL Please",icon="warning",fade_in_duration=5)   
-
